705b7dd393f3303f9b590d564cb09048b5f1b1c8
Ian Liu-Johnston <ian.liu-johnson@holbertonschool.com>
2016-12-13 17:09:23 -0800

    Added empty builtin functions.

---
+++ builtins.c
@@ -0,0 +1,26 @@
+#include "builtins.h"
+void _env(void)
+{
+
+}
+/**
+  * Functions below are not mandatory
+  *
+  *
+  */
+void _cd(void)
+{
+
+}
+void _alias(void)
+{
+
+}
+void _history(void)
+{
+
+}
+void help(void)
+{
+
+}
---
+++ builtins.h
@@ -0,0 +1,10 @@
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+void _env(void);
+void _cd(void);
+void _alias(void);
+void history(void);
+void help(void);
+
+#endif
