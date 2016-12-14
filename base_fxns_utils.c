af443c3f19ac2a008d19265f7d95889347cb9e8f
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-14 19:49:07 +0000

    added own implementation of malloc

--- base_fxns_utils.c
+++ base_fxns_utils.c
@@ -4,6 +4,48 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * _realloc - reallocates memory based on inputs
+ *
+ * @old_size: old memory size
+ * @new_size: new memory size
+ * @ptr: pointer to original memory address
+ * Return: new memory address
+ */
+void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *tmp, *buff;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	buff = malloc(new_size);
+	if (buff == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (buff);
+	}
+	tmp = ptr;
+	for (i = 0; i < old_size; i++)
+	{
+		buff[i] = tmp[i];
+	}
+	for (; i < new_size; i++)
+		buff[i] = '\0';
+	free(ptr);
+	return (buff);
+}
+
 void _memset(char *str, int fill, int n)
 {
 	int i;
@@ -12,8 +54,8 @@ void _memset(char *str, int fill, int n)
 }
 char * _getline(int file)
 {
-        int i;
-	char *buffer, *temp;
+        unsigned int i;
+	char *buffer;
 	static unsigned int total = 0;
 	unsigned int buffer_size = 1024;
 
@@ -28,12 +70,7 @@ char * _getline(int file)
 	total += i;
 	while (i >= 1024)
 	{
-		buffer_size += 1024;
-		temp = malloc((buffer_size) * sizeof(char));
-		_memset(temp, '\0', buffer_size);
-		memcpy(temp, buffer, (buffer_size - 1024));
-		free(buffer);
-		buffer = temp;
+		_realloc(buffer, buffer_size, buffer_size + 1024);
 		i = read(STDIN_FILENO, buffer + (buffer_size - 1024), 1024);
 		total += i;
 	}
