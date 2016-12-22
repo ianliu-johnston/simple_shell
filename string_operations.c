0c8871d66b54695301c68b98d91b8a072642aa76
davidknoppers <david.knoppers@holbertonschool.com>
2016-12-22 05:33:27 +0000

    fixing typo in or statement

--- string_operations.c
+++ string_operations.c
@@ -21,9 +21,10 @@ int _strlen(char *s)
  */
 int _strncmp(char *s1, char *s2, size_t bytes)
 {
-	if (s1 == NULL | s2 == NULL)
-		return (-1);
 	unsigned int i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (-1);
 
 	for (i = 0; s1[i] && s2[i] && s2[i] == s1[i] && i < bytes - 1; i++)
 		;
